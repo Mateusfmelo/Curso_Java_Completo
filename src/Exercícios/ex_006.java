18c3deec416b837092bec0368eb6762b90886e41
Mateus Melo <62157751+Mateusfmelo@users.noreply.github.com>
2022-08-29 19:25:56 -0300

    Update ex_006.java

--- src/Exercícios/ex_006.java
+++ src/Exercícios/ex_006.java
@@ -21,8 +21,8 @@ public class ex_006 {
         double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
 
         A = sc.nextDouble();
-		B = sc.nextDouble();
-		C = sc.nextDouble();
+	B = sc.nextDouble();
+	C = sc.nextDouble();
 
         triangulo = A * C / 2.0;
         circulo = 3.14159 * C * C;
@@ -31,11 +31,11 @@ public class ex_006 {
         retangulo = A * B;
 
         System.out.printf("TRIANGULO: %.3f%n", triangulo);
-		System.out.printf("CIRCULO: %.3f%n", circulo);
-		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
-		System.out.printf("QUADRADO: %.3f%n", quadrado);
-		System.out.printf("RETANGULO: %.3f%n", retangulo);
+	System.out.printf("CIRCULO: %.3f%n", circulo);
+	System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+	System.out.printf("QUADRADO: %.3f%n", quadrado);
+	System.out.printf("RETANGULO: %.3f%n", retangulo);
 		
-		sc.close();
+	sc.close();
     }
 }
