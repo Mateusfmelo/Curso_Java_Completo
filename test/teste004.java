c6d578286e2729df36cd3ae024e7581518fe6e1b
mateusfmelo <mateusmelo51@gmail.com>
2022-10-11 14:42:05 -0300

    Add files via upload

---
+++ test/teste004.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.Scanner;
+
+public class teste004 {
+
+    public static void main(String[] args) {
+
+        // Estrutura while
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while (x != 0) {
+            soma += x;
+            x = sc.nextInt();
+        }
+        
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
