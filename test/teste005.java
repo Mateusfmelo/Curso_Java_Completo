127fa3c7318642e31bf62bc529627c74885e2ca5
mateusfmelo <mateusmelo51@gmail.com>
2022-10-12 20:47:52 -0300

    Add file via upload

---
+++ test/teste005.java
@@ -0,0 +1,22 @@
+package test;
+
+//import java.util.Scanner;
+
+public class teste005 {
+
+    public static void main (String [] args){
+
+        //Scanner sc = new Scanner(System.in);
+
+        int x, y;
+
+        x = 0;
+        y = 4;
+        
+        while (x < 3){
+            x += 2;
+            y += 1;
+            System.out.println(x + "-" + y);
+        }
+    }
+}
